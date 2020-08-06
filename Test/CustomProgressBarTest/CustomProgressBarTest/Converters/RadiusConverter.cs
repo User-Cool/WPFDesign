d2fdb2253858e74c229a85068d6056284011f05d
User-Cool <34473770+User-Cool@users.noreply.github.com>
2020-08-06 16:10:18 +0800

    创建CustomProgressBarTest

---
+++ Test/CustomProgressBarTest/CustomProgressBarTest/Converters/RadiusConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CustomProgressBarTest
+{
+    /// <summary>
+    /// Radius 。类型是 double
+    /// </summary>
+    class RadiusConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double original1 = values[0] is double v1 ? v1 : default;
+            double original2 = values[1] is double v2 ? v2 : default;
+            double result = Math.Min(original1, original2);
+
+            if (targetType == typeof(double))
+                return result / 2.0;
+            else
+                return new CornerRadius(result / 2.0);
+        }
+
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
